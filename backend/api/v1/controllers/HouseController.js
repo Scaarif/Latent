16b371322cbba624d0c151ce22a8522690f29410
Gideon Obiasor <gideonoiasor@gmail.com>
2023-07-04 23:21:15 +0100

    Implemented the update method of houses route

--- backend/api/v1/controllers/HouseController.js
+++ backend/api/v1/controllers/HouseController.js
@@ -104,22 +104,104 @@ class HouseController {
     }
     try {
       const result = await House.find(params);
-      console.log(params);
       return res.status(200).json(result);
     } catch (err) {
       return res.status(400).json({ error: err.message });
     }
   }
 
+  /**
+   * Delete a house entry based on the provided ID.
+   * @param {Object} req - The HTTP request object.
+   * @param {Object} res - The HTTP response object.
+   * @returns {Promise} - A Promise that resolves to the deleted house entry or rejects with an error.
+   */
   static async deleteHouse(req, res) {
     try {
       // if (!req.user) return res.status(401).json({ error: "Unauthorized" });
       // const { _id } = req.user;
       const { _id } = req.query;
+
+      // Find and delete the house entry in the database with the provided ID.
       const result = await House.findByIdAndDelete(_id);
       return res.status(200).json(result);
     } catch (err) {
+      // If an error occurs during house deletion, return a JSON response with a 400 status code and the error message.
       return res.status(400).json({ error: err.message });
+    }
+  }
+
+  /**
+   * Updates an existing house entry in the database.
+   * @param {Object} req - The request object.
+   * @param {Object} res - The response object.
+   */
+  static async updateHouse(req, res) {
+    try {
+      if (!req.user) return res.status(401).json({ error: "Unauthorized" });
+      const { _id } = req.user;
+      // const { _id } = req.params;
+      // Add a check to confirm whether the authenticated user is the owner of the house
+      // ...
+      const {
+        country,
+        state,
+        city,
+        description,
+        price,
+        numFloors,
+        numRooms,
+        numBathrooms,
+        numToilets,
+        shared,
+        water,
+        electricity,
+        name,
+        address,
+        houseType,
+      } = req.body;
+
+      // Check if any images were uploaded.
+      if (req.files.length === 0) {
+        return res.status(400).json({ error: "No images uploaded" });
+      }
+
+      const uploadedImages = req.files.map((file) => file.path);
+
+      // Create an object with the properties to be updated in the database.
+      const updateObject = {
+        location: { country, state, city },
+        description,
+        price,
+        numFloors,
+        numRooms,
+        numBathrooms,
+        numToilets,
+        shared,
+        water,
+        electricity,
+        name,
+        address,
+        houseType,
+        coverImage: uploadedImages[0],
+        images: uploadedImages.slice(1),
+      };
+
+      // Find the existing house by ID and update it with the new data.
+      const existingHouse = await House.findByIdAndUpdate(
+        _id,
+        updateObject,
+        { new: true } // Return the updated house after the update is applied.
+      );
+
+      // If the house doesn't exist, return a 404 response.
+      if (!existingHouse) {
+        return res.status(404).json({ error: "House not found" });
+      }
+
+      res.status(200).json(existingHouse);
+    } catch (err) {
+      res.status(400).json({ error: err.message });
     }
   }
 }
--- backend/api/v1/routes/index.js
+++ backend/api/v1/routes/index.js
@@ -12,4 +12,9 @@ router.post(
 
 router.get("/api/v1/houses", HouseController.getHouse);
 router.delete("/api/v1/houses", HouseController.deleteHouse);
+router.put(
+  "/api/v1/houses",
+  upload.array("images", 5),
+  HouseController.updateHouse
+);
 module.exports = router;
