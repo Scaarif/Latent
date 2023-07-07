54958f4b1aa85a37186eedc1813ce04838b8f6ad
Gideon Obiasor <gideonoiasor@gmail.com>
2023-07-07 22:10:31 +0100

    Implemented email services

--- backend/api/v1/routes/index.js
+++ backend/api/v1/routes/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const HouseController = require('../controllers/HouseController');
-const upload = require('../middlewares/upload');
-const { Tenant, Agent } = require('../models/User');
+const upload = require('../middelwares/upload');
 
 const router = express.Router();
 
@@ -13,27 +12,13 @@ router.post(
   HouseController.postHouse,
 );
 
-router.get('/api/v1/houses', HouseController.getHouse);
-router.delete('/api/v1/houses', HouseController.deleteHouse);
+router.get('/houses', HouseController.getHouse);
+router.delete('/houses', HouseController.deleteHouse);
 router.put(
-  '/api/v1/houses',
+  '/houses',
   upload.array('images', 5),
   HouseController.updateHouse,
 );
-router.post('/api/v1/appointment/:houseId', HouseController.bookHouse);
-
-router.post('/api/v1/tenant', async (req, res) => {
-  const { firstName, phoneNumber, email } = req.body;
-  const tenant = new Tenant({ firstName, phoneNumber, email });
-  const result = await tenant.save();
-  res.status(201).json(result);
-});
-
-router.post('/api/v1/agent', async (req, res) => {
-  const { firstName, phoneNumber, email } = req.body;
-  const agent = new Agent({ firstName, phoneNumber, email });
-  const result = await agent.save();
-  res.status(201).json(result);
-});
+router.post('/appointment/:houseId', HouseController.bookHouse);
 
 module.exports = router;
