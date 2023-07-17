61c3afa04fd21d3cc39bddda784bbbf5344f92f6
Gideon Obiasor <gideonoiasor@gmail.com>
2023-07-17 11:34:58 +0100

    Changed the pattern of saving images from paths to urls

---
+++ backend/api/v1/utils/sendEmail.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail', // Specify the email service provider (e.g., Gmail)
+  auth: {
+    user: process.env.EMAIL, // Your email address
+    pass: process.env.EMAIL_APP_PASSWORD, // Your email password
+  },
+});
+
+const sendEmail = async (mailOptions) => {
+  try {
+    const info = await transporter.sendMail(mailOptions);
+    console.log('Email sent successfully!', info.response);
+  } catch (error) {
+    console.error('Error sending email:', error);
+  }
+};
+
+module.exports = sendEmail;
