2952c1eb9fc8d896fc1abc70be0e4a1d7fcb1f6e
Gideon Obiasor <gideonoiasor@gmail.com>
2023-07-18 16:59:40 +0100

    Changed the emailQueue job data to send only OTP and also formattede the messages being sent to emails

--- backend/api/v1/jobs/processJobs.js
+++ backend/api/v1/jobs/processJobs.js
@@ -29,45 +29,78 @@ bookHouseQueue.process(async (job) => {
     if (!tenant) throw new Error('No tenant found');
     const agent = await Agent.findById(agentId);
     if (!agent) throw new Error('No agent found');
+    console.log(agent)
 
-    const tenantMessage = `Hello ${tenant.firstName}\nYou have indicated interest in inspecting a house listed by ${agent.firstName}\n\nHouse details:\n\tAddress: ${houseAddress}\n\tDescription: ${houseDescription}\n\tAgent/Owner's contact: ${agent.phone}\n\nPlease kindly contact the agent\nThank you for choosing Latent for your housing services`;
-    const agentMessage = `Hello ${agent.firstName}\nThere is a potential tenant by name ${tenant.firstName} who is interested in your house.\nHouse details:\n\tAddress: ${houseAddress}\n\tDescription: ${houseDescription}\n\nYour contact has been shared with the tenant\nThank you for choosing Latent for your housing services`;
+    // Format the tenant message with inline CSS
+    const tenantMessage = `
+      <h2 style="color: #007bff;">Hello ${tenant.firstName},</h2>
+      <p>You have indicated interest in inspecting a house listed by ${agent.firstName}.</p>
+      <h3>House Details:</h3>
+      <p><strong>Address:</strong> ${houseAddress}</p>
+      <p><strong>Description:</strong> ${houseDescription}</p>
+      <p><strong>Agent/Owner's contact:</strong> ${agent.phone}</p>
+      <p>Please kindly contact the agent.</p>
+      <p>Thank you for choosing Latent for your housing services.</p>
+    `;
 
-    const mailOptions = [{
-      from: `Latent ${process.env.EMAIL}`,
-      to: tenant.email,
-      subject: 'House inspection',
-      text: `${tenantMessage}`,
-    },
-    {
-      from: `Latent ${process.env.EMAIL}`,
-      to: agent.email,
-      subject: 'House inspection',
-      text: `${agentMessage}`,
-    }];
+    // Format the agent message with inline CSS
+    const agentMessage = `
+      <h2 style="color: #007bff;">Hello ${agent.firstName},</h2>
+      <p>There is a potential tenant by the name of ${tenant.firstName} who is interested in your house.</p>
+      <h3>House Details:</h3>
+      <p><strong>Address:</strong> ${houseAddress}</p>
+      <p><strong>Description:</strong> ${houseDescription}</p>
+      <p>Your contact has been shared with the tenant.</p>
+      <p>Thank you for choosing Latent for your housing services.</p>
+    `;
+
+    const mailOptions = [
+      {
+        from: `Latent ${process.env.EMAIL}`,
+        to: tenant.email,
+        subject: 'House inspection',
+        html: tenantMessage, // Set the tenant message to use HTML
+      },
+      {
+        from: `Latent ${process.env.EMAIL}`,
+        to: agent.email,
+        subject: 'House inspection',
+        html: agentMessage, // Set the agent message to use HTML
+      },
+    ];
+
     console.log(mailOptions);
-    mailOptions.forEach(async (mailOption) => {
+
+    // Using for...of loop to ensure asynchronous email sending
+    for (const mailOption of mailOptions) {
       await sendEmail(mailOption);
-    });
+    }
   } catch (err) {
     console.error(err.message);
   }
 });
 
+
+
 // Handle sending of OTP to user email for password reset
 resetPassword.process(async (job) => {
   try {
-    const {
-      email, otp, firstName, lastName,
-    } = job.data;
-    const message = `Click the link below to reset your password\n\nhttp://localhost:5000/api/v1/reset-password?email=${email}&otp=${otp}&firstName=${firstName}&lastName=${lastName}`;
-    const mailOptionS = {
+    const { email, otp } = job.data;
+
+    // Format the email message with HTML
+    const message = `<p>This is your one-time password (OTP) for resetting your Latent login password:</p>
+                     <p style="font-weight: bold; font-size: 16px;">${otp}</p>
+                     <p>This OTP is valid only for 10 minutes.</p>
+                     <p>Thank you for choosing Latent for your housing needs and services!</p>`;
+
+    const mailOptions = {
       from: `Latent ${process.env.EMAIL}`,
       to: email,
       subject: 'Password reset',
-      text: message,
+      html: message, // Set the email body to use HTML
     };
-    await sendEmail(mailOptionS);
+
+    await sendEmail(mailOptions);
   } catch (err) {
     console.log(err.message);
   }
